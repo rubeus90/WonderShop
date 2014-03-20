0f20a05da90a97e4ba3ab138828d27b29f512085
rubeus90 <rubeus90@gmail.com>
2014-03-20 10:43:19 +0100

    Ajouter une session a chaque controleur

--- src/java/pkgControleur/ConnexionControleur.java
+++ src/java/pkgControleur/ConnexionControleur.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -23,6 +24,7 @@ public class ConnexionControleur extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
